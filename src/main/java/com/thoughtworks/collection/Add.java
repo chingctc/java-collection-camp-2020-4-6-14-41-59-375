5b5d1a335797a42a2907864171d60a75a5d36c86
Mavis Chan <mavis.chan@oocl.com>
2020-04-07 14:41:54 +0800

    Java - Simple exercise
    - Composed "Add.java"

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Add {
@@ -19,19 +20,69 @@ public class Add {
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> newNumberList = new ArrayList<Integer>();
+
+        for (int index = 0; index < arrayList.size(); index++) {
+            if (arrayList.get(index) % 2 != 0) {
+                newNumberList.add((arrayList.get(index) * 3) + 2);
+            } else {
+                newNumberList.add(arrayList.get(index));
+            }
+        }
+//        throw new NotImplementedException();
+        return newNumberList;
+//        throw new NotImplementedException();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sumOfList = 0;
+
+        for (int index = 0; index < arrayList.size(); index++) {
+            int newListAfterCal = (arrayList.get(index) * 3) + 2;
+            sumOfList += newListAfterCal;
+            System.out.println(sumOfList);
+        }
+//        throw new NotImplementedException();
+        return sumOfList;
     }
 
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sumEvenNumber = 0;
+
+        if(leftBorder < rightBorder) {
+            for (int start = leftBorder; start <= rightBorder; start++) {
+                if (start % 2 == 0) {
+                    sumEvenNumber += start;
+                }
+            }
+        }else {
+            for (int start = rightBorder; start <= leftBorder; start++) {
+                if (start % 2 == 0) {
+                    sumEvenNumber += start;
+                }
+            }
+        }
+        return sumEvenNumber;
+//        throw new NotImplementedException();
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
-    }
+        int sumOddNumber = 0;
 
+        if (leftBorder < rightBorder) {
+            for (int start = leftBorder; start <= rightBorder; start++) {
+                if (start % 2 != 0) {
+                    sumOddNumber += start;
+                }
+            }
+        } else {
+            for (int start = rightBorder; start <= leftBorder; start++) {
+                if (start % 2 != 0) {
+                    sumOddNumber += start;
+                }
+            }
+        }
+        return sumOddNumber;
+//        throw new NotImplementedException();
+    }
 }
